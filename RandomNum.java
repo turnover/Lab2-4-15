90436e5057f869181f87b874887d211fb2fd3f93
turnover <counterparts714@gmail.com>
2014-04-15 21:25:52 -0700

    replay

--- RandomNum.java
+++ RandomNum.java
@@ -2,7 +2,7 @@
 public class RandomNum 
 {
 	public int computerNum1;
-	public int GetANumber_Between_1_and_10(int high)
+	public int GetANumber_Between_1_and_High(int high)
 	{
 		computerNum1 = 1 + (int) (Math.random()*high);
 		return computerNum1;
