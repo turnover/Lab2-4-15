cdd07c291a6b2bf742016e70f4ef627ebb64568e
turnover <counterparts714@gmail.com>
2014-04-15 21:19:12 -0700

    redone

--- RandomNum.java
+++ RandomNum.java
@@ -7,5 +7,9 @@ public class RandomNum
 		computerNum1 = 1 + (int) (Math.random()*high);
 		return computerNum1;
 	}
-	
+	public int GetANumber_Between_1_and_10()
+	{
+		computerNum1 = 1 + (int) (Math.random()*10);
+		return computerNum1;
+	}
 }
